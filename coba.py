84b82b1d042c11268223c2d2d15856aafe1d243a
Irwansyah <109840077+iwplus@users.noreply.github.com>
2023-07-14 08:39:41 +0800

    Update coba.py

--- coba.py
+++ coba.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df=pd.read_csv('D:/data3.csv', sep=';')
+df=pd.read_csv('D:/data3.csv', sep=';') ### example of reading data, change it when necessary 
 data=np.array(df)
 n=int(input('banyaknya variabel = '))
 x=np.array(data[:,1:n+1]).reshape(len(data),n)
@@ -12,24 +12,26 @@ y=np.array(data[:,0]).reshape(len(data),1)
 
 ldata=len(data)
 
-#Uji Kernel
-#h=Nonparametrik.gen_band_awal(0.1,4,x)
-#a,b,c,d = Nonparametrik.cari_band_opt(h,x,y,4,Nonparametrik.gcv)
-#Nonparametrik.gambarkernel(y,c)
+### Try kernel regression ####
 
-#f=pd.DataFrame(c)
-#f.to_csv('azizi12.csv',index=None,header=True)
+h=Nonparametrik.gen_band_awal(0.1,4,x)
+a,b,c,d = Nonparametrik.cari_band_opt(h,x,y,4,Nonparametrik.gcv)
+Nonparametrik.gambarkernel(y,c)
 
-#uji spline
+f=pd.DataFrame(c)
+f.to_csv('output.csv',index=None,header=True)
+
+#### try spline truncated regression ####
+
 mmax,mmin=Nonparametrik.maxmin(x)
 knots=Nonparametrik.bangkitkan_knot(mmax,mmin,1,n,x)
 a,b,c,d=Nonparametrik.cari_knot_opt(knots,x,y,mmax,Nonparametrik.gcvknot,ldata,2,df)
 Nonparametrik.gambarspline(y,c)
 
-###coba dourier
-#nknots=Nonparametrik.bangkitkan_jumlah_knot()
-#print('jumlah knots  = ',nknots)
-#a,b,c,d=Nonparametrik.cari_nknot_opt(nknots,x,y,Nonparametrik.gcvfourier,n,data)
-#Nonparametrik.gambarfourier(y,c)
-#f=pd.DataFrame(c)
-#f.to_csv('databaruu1.csv',index=None,header=True)
+##### try Fourier regression
+nknots=Nonparametrik.bangkitkan_jumlah_knot()
+print('jumlah knots  = ',nknots)
+a,b,c,d=Nonparametrik.cari_nknot_opt(nknots,x,y,Nonparametrik.gcvfourier,n,data)
+Nonparametrik.gambarfourier(y,c)
+f=pd.DataFrame(c)
+f.to_csv('output2.csv',index=None,header=True)
